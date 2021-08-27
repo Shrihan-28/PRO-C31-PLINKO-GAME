1e4a538ef81867407938df175bb5ca80fdfb9f80
Shrihan-28 <77265902+Shrihan-28@users.noreply.github.com>
2021-08-27 12:08:56 +0530

    Update Plinko.js

--- Plinko.js
+++ Plinko.js
@@ -7,7 +7,7 @@ class Plinko {
         }
         this.r = 10;
         this.body = Bodies.circle(x, y, this.r, options);
-        
+        this.colour = color(random(0,255),random(0,255),random(0,255))
         World.add(world, this.body);
     }
     display() {
